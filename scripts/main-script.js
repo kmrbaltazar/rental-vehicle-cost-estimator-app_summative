08c6cb1ef2c4355bd9062c07adcf80c16d683bcf
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2022-11-30 20:48:09 +1300

    Header and Landing sections responsive
    Works on desktop, tablet, mobile phones (except Samsung Galaxy Fold)

---
+++ scripts/main-script.js
@@ -0,0 +1,5 @@
+// BURGER MENU FOR TABLET AND MOBILE
+document.getElementById('burger-menu').onclick = function(){
+    let burger_nav = document.getElementById('burger-nav');
+    burger_nav.classList.toggle('no-display');
+}
