0738b3638209c94f43963c6f33b6643c2018981d
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2022-12-02 18:50:54 +1300

    Flatpickr applied

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,5 +1,17 @@
 // BURGER MENU FOR TABLET AND MOBILE
-document.getElementById('burger-menu').onclick = function(){
-    let burger_nav = document.getElementById('burger-nav');
-    burger_nav.classList.toggle('no-display');
-}
+document.getElementById("burger-menu").onclick = function () {
+  let burger_nav = document.getElementById("burger-nav");
+  burger_nav.classList.toggle("no-display");
+};
+
+// FLATPICKR
+const new_calendar = {
+  mode: "range",
+  minDate: "today",
+  dateFormat: "y-M-d",
+  altInput: true,
+  altFormat: "d-M-y",
+  allowInput: true,
+};
+
+flatpickr("#date-range", new_calendar);
